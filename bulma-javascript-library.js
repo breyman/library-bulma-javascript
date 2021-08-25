63cea9f266e1b2896d9cfa45cbf6e2ed7c62cdd7
Brian Reyman <brian@Brians-MacBook-Pro.local>
2021-08-25 06:05:57 -0600

    Update Tab Function to More Slowly Iterate Through Things

--- bulma-javascript-library.js
+++ bulma-javascript-library.js
@@ -7,26 +7,28 @@
 // adjust Bulma tabs to show which is currently active
 // need to name the container element id="bulma-tabs" at the div container levle and each tab class="bulma-tab" at the li level
 (function() {
-	// Get the container element
-	let bulmaTabContainer = document.getElementById('bulma-tabs');
+	let myBulmaTabsContainer = document.getElementById('bulma-tabs');
+	let myBulmaTabs = myBulmaTabsContainer.getElementsByClassName('bulma-tab');
 
-	// Get all tabs with inside the container
-	let bulmaTabs = bulmaTabContainer.getElementsByClassName('bulma-tab');
-
-	// Loop through the buttons and add the active class to the current/clicked button
-	for (var i = 0; i < bulmaTabs.length; i++) {
-		bulmaTabs[i].addEventListener('click', function() {
+	let n = myBulmaTabs.length;
+	let makeHandler = function(num) {
+		// outer function
+		return function() {
+			// inner function
 			let onBulmaTabContainer = document.getElementById('bulma-tabs');
 
 			// Get all buttons with class="btn" inside the container
 			let onBulmaTabs = onBulmaTabContainer.getElementsByClassName('bulma-tab');
 
 			// Loop through the buttons and add the active class to the current/clicked button
-			for (var z = 0; z < bulmaTabs.length; z++) {
+			for (var z = 0; z < onBulmaTabs.length; z++) {
 				onBulmaTabs[z].className = onBulmaTabs[z].className.replace(' is-active', '');
 			}
 
 			this.className += ' is-active';
-		});
+		};
+	};
+	for (var i = 0; i < n; i++) {
+		myBulmaTabs[i].onclick = makeHandler(i + 1);
 	}
 })();
