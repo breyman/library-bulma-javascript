b0441fd20bb5f53dd627d6f12276f075fc7d0e74
Brian Reyman <brian@Brians-MacBook-Pro.local>
2021-08-31 11:26:17 -0600

    Adding Error Checking

--- bulma-javascript-library.js
+++ bulma-javascript-library.js
@@ -6,29 +6,31 @@
 
 // adjust Bulma tabs to show which is currently active
 // need to name the container element id="bulma-tabs" at the div container levle and each tab class="bulma-tab" at the li level
-(function() {
-	let myBulmaTabsContainer = document.getElementById('bulma-tabs');
-	let myBulmaTabs = myBulmaTabsContainer.getElementsByClassName('bulma-tab');
+if (document.getElementById('bulma-tabs') != null && typeof document.getElementById('bulma-tabs') != 'undefined') {
+	(function() {
+		let myBulmaTabsContainer = document.getElementById('bulma-tabs');
+		let myBulmaTabs = myBulmaTabsContainer.getElementsByClassName('bulma-tab');
 
-	let n = myBulmaTabs.length;
-	let makeTabButtonsHandler = function(num) {
-		// outer function
-		return function() {
-			// inner function
-			let onBulmaTabContainer = document.getElementById('bulma-tabs');
+		let n = myBulmaTabs.length;
+		let makeTabButtonsHandler = function(num) {
+			// outer function
+			return function() {
+				// inner function
+				let onBulmaTabContainer = document.getElementById('bulma-tabs');
 
-			// Get all buttons with class="btn" inside the container
-			let onBulmaTabs = onBulmaTabContainer.getElementsByClassName('bulma-tab');
+				// Get all buttons with class="btn" inside the container
+				let onBulmaTabs = onBulmaTabContainer.getElementsByClassName('bulma-tab');
 
-			// Loop through the buttons and add the active class to the current/clicked button
-			for (var z = 0; z < onBulmaTabs.length; z++) {
-				onBulmaTabs[z].className = onBulmaTabs[z].className.replace(' is-active', '');
-			}
+				// Loop through the buttons and add the active class to the current/clicked button
+				for (var z = 0; z < onBulmaTabs.length; z++) {
+					onBulmaTabs[z].className = onBulmaTabs[z].className.replace(' is-active', '');
+				}
 
-			this.className += ' is-active';
+				this.className += ' is-active';
+			};
 		};
-	};
-	for (var i = 0; i < n; i++) {
-		myBulmaTabs[i].onclick = makeTabButtonsHandler(i + 1);
-	}
-})();
+		for (var i = 0; i < n; i++) {
+			myBulmaTabs[i].onclick = makeTabButtonsHandler(i + 1);
+		}
+	})();
+}
